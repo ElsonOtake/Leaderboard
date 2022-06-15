93b6e8cffd97ab31ae4713a51574eda085e94e2c
Elson Otake <elsonotake@yahoo.com.br>
2022-06-15 16:56:59 -0300

    Update loadScores calling loadList

--- src/load-scores.js
+++ src/load-scores.js
@@ -1,11 +1,13 @@
 import gameId from "./game-id";
+import loadList from "./load-list";
 
 const loadScores = () => {
   const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/' + gameId() + '/scores/';
   const request = new Request(requestURL);
+  console.log("loadScores");
   fetch(request)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => loadList(data));
 }
 
 export default loadScores;
