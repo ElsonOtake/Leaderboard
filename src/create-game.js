c65d0a1e783dfb26c1f8e432b07de10ff4d1ba2c
Elson Otake <elsonotake@yahoo.com.br>
2022-06-15 12:39:35 -0300

    Update createGame

--- src/create-game.js
+++ src/create-game.js
@@ -1,5 +1,16 @@
-const createGame = () => {
+import postData from "./post-data";
 
+const bigBamboo = {
+  id: ''
+};
+
+const createGame = () => {
+  postData('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', { name: "Big bamboo game" })
+  .then(data => {
+    bigBamboo.id = data.result.substr(14, 20);
+    const gameData = JSON.stringify(bigBamboo);
+    localStorage.setItem('gameId', JSON.stringify(gameData));
+  });
 }
 
 export default createGame;
