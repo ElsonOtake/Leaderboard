94ff022db41fd6cccc73c3c83cac45df1a7b8684
Elson Otake <elsonotake@yahoo.com.br>
2022-06-15 12:21:30 -0300

    Add postData module

---
+++ src/post-data.js
@@ -0,0 +1,12 @@
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(data)
+  });
+  return response.json();
+}
+
+export default postData;
