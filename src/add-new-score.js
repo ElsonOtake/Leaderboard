a0930b24eedde8e566dfd714bcd129cbf2b6db6d
Elson Otake <elsonotake@yahoo.com.br>
2022-06-15 17:26:52 -0300

    Add addNewScore function

---
+++ src/add-new-score.js
@@ -0,0 +1,29 @@
+import addScore from "./add-score";
+
+const addNewScore = () => {
+  const submitBtn = document.querySelector('.add-score button');
+  const inputTxt = document.querySelector('.add-score input[type=text]');
+  const inputNum = document.querySelector('.add-score input[type=number]');
+
+  const enableButton = () => {
+    if (inputTxt.value !== '' && inputNum.value !== '') {
+      submitBtn.disabled = false;
+    } else {
+      submitBtn.disabled = true;
+    }
+  }
+
+  submitBtn.addEventListener('click', () => {
+    addScore(inputTxt.value, parseInt(inputNum.value, 10));
+  })
+
+  inputTxt.addEventListener('input', () => {
+    enableButton();
+  })
+
+  inputNum.addEventListener('input', () => {
+    enableButton();
+  })
+}
+
+export default addNewScore;
